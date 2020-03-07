710a07228f8c4fc56ce35661fe6498753aa3a576
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-03-07 06:44:06 -0600

    ENH first set of outputs (#1)
    * ENH first set of outputs
    
    * added osx
    
    * reverse mapping add osx
    
    * got conda-smithy
    
    * more tbtf stuff

---
+++ scripts/gen_tbtf_nodes.py
@@ -0,0 +1,78 @@
+import networkx as nx
+from conda_forge_tick.utils import load_graph
+
+
+def _get_feedstock(p, gx, output_to_feedstock):
+    fs = None
+    if p in gx.nodes:
+        _p = p
+    elif (
+        p in output_to_feedstock and
+        output_to_feedstock[p] in gx.nodes
+    ):
+        _p = output_to_feedstock[p]
+    else:
+        print("UNKNOWN OUTPUT: pkg=%s output=%s" % (pkg, p))
+
+    if not gx.nodes[_p]["payload"].get("archived", False):
+        fs = _p
+
+    return fs
+
+
+# 1. load the graph
+gx = load_graph()
+
+# 2. constrct mapping of outputs to feedstocks
+output_to_feedstock = {
+    output: name
+    for name, node in gx.nodes.items()
+    for output in node.get("payload", {}).get("outputs_names", [])
+}
+
+# 3. get deps of key packages and big deps
+init_tbtf_packages = set()
+
+# get deps of some key packages
+for pkg in ["conda", "conda-build", "conda-smithy"]:
+    _tbtf = set()
+    with gx.nodes[pkg]["payload"] as attrs:
+        for _, v in attrs["requirements"].items():
+            for p in v:
+                fs = _get_feedstock(p, gx, output_to_feedstock)
+                if fs is not None:
+                    _tbtf.add(fs)
+
+    if pkg not in _tbtf:
+        fs = _get_feedstock(pkg, gx, output_to_feedstock)
+        if fs is not None:
+            _tbtf.add(fs)
+    init_tbtf_packages |= _tbtf
+
+# more than 5% of everything
+percent = .05
+min_dec = int(len(gx) * percent)
+for n in gx.nodes:
+    if len(nx.descendants(gx, n)) > min_dec:
+        fs = _get_feedstock(n, gx, output_to_feedstock)
+        if fs is not None:
+            init_tbtf_packages.add(fs)
+
+# 4. now add all ancestors of those
+_anc = set()
+for p in init_tbtf_packages:
+    anc = nx.ancestors(gx, p)
+    for a in anc:
+        fs = _get_feedstock(a, gx, output_to_feedstock)
+        if fs is not None:
+            _anc.add(fs)
+
+tbtf_packages = _anc | init_tbtf_packages
+
+# 5. print and save
+for p in sorted(list(tbtf_packages)):
+    print("   ", p)
+
+with open("../feedstock-outputs/scripts/tbtf_nodes.txt", "w") as fp:
+    for p in sorted(list(tbtf_packages)):
+        fp.write("%s\n" % p)
---
+++ scripts/gen_tbtf_outputs.py
@@ -0,0 +1,99 @@
+import sys
+import pprint
+import requests
+import json
+
+from conda_forge_tick.utils import load_graph
+
+# 1. load the graph
+gx = load_graph()
+
+# 2. read the nodes
+with open("../feedstock-outputs/scripts/tbtf_nodes.txt", "r") as fp:
+    tbtf_nodes = [n.strip() for n in fp.readlines()]
+
+# 3. add things that seem to be missed...
+tbtf_nodes = set(tbtf_nodes)
+tbtf_nodes |= set([
+    "clang-compiler-activation",
+    "clang-win-activation",
+    "gfortran_osx-64",
+    "gfortran_impl_osx-64",
+    "_openmp_mutex",
+    "intel_repack",
+    "numba",
+    "cython",
+    "pybind11",
+])
+
+
+seen = set()
+tbtf_outputs = {}
+for n in tbtf_nodes:
+    tbtf_outputs[n] = set()
+    if "feedstock_name" not in gx.nodes[n]["payload"]:
+        print(n, gx.nodes[n]["payload"].data)
+        sys.exit(1)
+    assert n == gx.nodes[n]["payload"]["feedstock_name"]
+
+    outs = gx.nodes[n]["payload"].get("outputs_names", [])
+    if outs:
+        for out in outs:
+            tbtf_outputs[n].add(out)
+    else:
+        tbtf_outputs[n].add(n)
+
+    if tbtf_outputs[n] & seen:
+        for bado in tbtf_outputs[n] & seen:
+            print("OUTPUT CONFLICT: %s" % bado)
+            for k, v in tbtf_outputs.items():
+                if bado in tbtf_outputs[k]:
+                    print("    %s: %s" % (k, bado))
+
+    seen |= tbtf_outputs[n]
+
+# 3. now check to make sure we have the right names
+tails = ["linux-64", "win-64", "osx-64", "linux-aarch64", "linux-ppc64le"]
+for p in tbtf_outputs:
+    final_outs = set()
+    for out in tbtf_outputs[p]:
+        if out.endswith("_"):
+            for tail in tails:
+                # a little special casing
+                if p.startswith('ctng') and tail == 'osx-64':
+                    continue
+
+                if p == "clang-compiler-activation" and tail == "win-64":
+                    continue
+
+                r = requests.get(
+                    "https://api.anaconda.org/package/conda-forge/%s%s" % (
+                        out, tail))
+                if r.status_code == 200:
+                    final_outs |= set([out + tail])
+
+            r = requests.get(
+                "https://api.anaconda.org/package/conda-forge/%s%s" % (
+                    out, ""))
+            if r.status_code == 200:
+                final_outs |= set([out])
+        else:
+            final_outs.add(out)
+    if final_outs != tbtf_outputs[p]:
+        print("MUNGED NEW OUTPUTS:", p)
+    tbtf_outputs[p] = final_outs
+
+with open("../feedstock-outputs/scripts/final_outputs.txt", "w") as fp:
+    fp.write(pprint.pformat(tbtf_outputs))
+
+# 4. now reverse the mapping and write out
+rev = {}
+for p, outs in tbtf_outputs.items():
+    for out in outs:
+        if out not in rev:
+            rev[out] = set()
+        rev[out].add(p)
+
+for p in rev:
+    with open("../feedstock-outputs/outputs/%s.json" % p, "w") as fp:
+        json.dump({"feedstocks": list(rev[p])}, fp)
